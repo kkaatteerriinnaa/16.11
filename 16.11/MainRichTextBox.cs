e6102343e9492ee7da1c93224426c0904fe33518
kkaat <kkaat@KATERINA>
2023-12-27 01:12:28 +0200

    Добавьте файлы проекта.

---
+++ 16.11/MainRichTextBox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace _16._11
+{
+    public partial class MainWindow
+    {
+        private RichTextBox MainRichTextBox;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            MainRichTextBox = new RichTextBox
+            {
+                Name = "MainRichTextBox",
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Stretch
+            };
+
+            // Добавление RichTextBox на главное окно
+            this.Content = MainRichTextBox;
+        }
+    }
+}
---
+++ 16.11/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _16._11
+{
+    public partial class MainWindow : Window
+    {
+        private void ApplyFormatting(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(StartIndexInput.Text, out int start) && int.TryParse(EndIndexInput.Text, out int end))
+            {
+                if (start >= 0 && end <= new TextRange(MainRichTextBox.Document.ContentStart, MainRichTextBox.Document.ContentEnd).Text.Length && start <= end)
+                {
+                    TextRange range = new TextRange(MainRichTextBox.Document.ContentStart, MainRichTextBox.Document.ContentEnd);
+                    range.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Normal);
+                    range.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Normal);
+
+                    TextPointer startPointer = MainRichTextBox.Document.ContentStart.GetPositionAtOffset(start, LogicalDirection.Forward);
+                    TextPointer endPointer = MainRichTextBox.Document.ContentStart.GetPositionAtOffset(end, LogicalDirection.Backward);
+                    TextRange selectedRange = new TextRange(startPointer, endPointer);
+
+                    if ((sender as Button).Name == "BoldButton")
+                        selectedRange.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
+                    else if ((sender as Button).Name == "ItalicButton")
+                        selectedRange.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Italic);
+                    else if ((sender as Button).Name == "UnderlineButton")
+                        selectedRange.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Underline);
+                }
+            }
+        }
+
+        private void ClearFormatting(object sender, RoutedEventArgs e)
+        {
+            TextRange range = new TextRange(MainRichTextBox.Document.ContentStart, MainRichTextBox.Document.ContentEnd);
+            range.ClearAllProperties();
+        }
+    }
+}
+
